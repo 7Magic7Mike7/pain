5f2fd9fbbdfa3ed22f82b96f1a45bd28e2d26e8f
Michael Artner <michael.artner@hotmail.de>
2026-07-12 14:17:00 +0200

    adapted temperature data to new db structure

--- scripts/AIO_prod/gen_temperature_dataset.py
+++ scripts/AIO_prod/gen_temperature_dataset.py
@@ -9,11 +9,12 @@ BASE_PATH = os.path.join("..", "..", "data")
 # where to find the raw dataset
 DATA_PATH = os.path.join(BASE_PATH, "raw", "Global_TAVG_Gridded_5deg.nc")
 # where to save the transformed dataset to
-OUT_PATH = os.path.join(BASE_PATH, "actual", "temperature_anomalies.csv")
+OUT_PATH = os.path.join(BASE_PATH, "actual", "temperature", "temperature_final.csv")
 # which year(s) we want to visualize
 YEAR_START = 2020
 YEAR_END = 2025
 
+print(f"0) Loading data from {DATA_PATH}")
 # 1) transform to DataFrame
 print("1) Transforming to DataFrame")
 ds = xr.open_dataset(DATA_PATH, engine="netcdf4")
@@ -33,7 +34,7 @@ df_filtered = df[df['temperature'].notnull()].reset_index(drop=True)
 
 # 2) normalize values
 print("2) Normalizing Values")
-def normalize_temperature_dataset(dataframe: pd.DataFrame, painorigin: str = "EnvNat") -> pd.DataFrame:
+def normalize_temperature_dataset(dataframe: pd.DataFrame, category: str = "Temperature") -> pd.DataFrame:
     max_temp, min_temp = dataframe['temperature'].max(), dataframe['temperature'].min()
     temp_range = max_temp - min_temp
     temp_offset = min_temp
@@ -45,14 +46,15 @@ def normalize_temperature_dataset(dataframe: pd.DataFrame, painorigin: str = "En
         value = (temp - temp_offset) / temp_range
         
         data.append({
-            'lat': lat,
-            'lon': lon,
+            'aggrId': None,
             'value': np.round(value, 5),
-            'datatype': 'Temperature',
-            'painorigin': painorigin
+            'category': category,
+            'lat': lat,
+            'lng': lon
         })
 
     return pd.DataFrame(data)
 df_temp = normalize_temperature_dataset(df_filtered)
+print(f"3) Saving data to {OUT_PATH}")
 df_temp.to_csv(OUT_PATH, index=True, index_label="id")
 print("-done-")
