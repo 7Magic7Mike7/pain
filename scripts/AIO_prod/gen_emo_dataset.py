df8e58b32ec33253a2adb390cb4f1cb8caf31103
Michael Artner <michael.artner@hotmail.de>
2026-08-16 17:16:43 +0200

    improve AIO for emo and socioeco

--- scripts/AIO_prod/gen_emo_dataset.py
+++ scripts/AIO_prod/gen_emo_dataset.py
@@ -1,9 +1,26 @@
+from typing import List
+import argparse
+import numpy as np
 import os
 import pandas as pd
+
+
+DEFAULT_BASE_PATH = os.path.join("data")#'..', '..', 'data')
+DEFAULT_INPUT_PATH = os.path.join(DEFAULT_BASE_PATH, "dummy", "emotion-values-dummy.csv")
+DEFAULT_WORDS_PATH = os.path.join(DEFAULT_BASE_PATH, "dummy", "emotion-mvp-words-dummy.csv")
+DEFAULT_OUTPUT_PATH = os.path.join(DEFAULT_BASE_PATH, "dummy", "data_types", "emo.csv")
 
 VALUE_COLUMNS = \
     "anger_moral_injury,climate_fear_anxiety_panic,depression_suicidal_ideation,displacement_exile,emotional_pain_unspecified,general_fear_anxiety_panic,general_physical_pain,grief_solastalgia,helplessness_powerlessness,loss_precarity,no_pain,out_of_scope,relationship_social_pain,shame_guilt_self_blame,trauma_overwhelm,uncertainty_instability" \
     .split(",")
+
+def _init_value_columns() -> List[str]:
+  # deep copy VALUE_COLUMNS
+  value_columns = [vc for vc in VALUE_COLUMNS]
+  # ignore no_pain & out_of_scope as they do not represent emotional pain
+  value_columns.remove("no_pain")
+  value_columns.remove("out_of_scope")
+  return value_columns
 
 def _load_words(words_path: str):
   df_words = pd.read_csv(words_path)
@@ -17,33 +34,65 @@ def _load_words(words_path: str):
 def perform(input_path: str, words_path: str, output_path: str):
   print("Starting...")
   word_by_country = _load_words(words_path)
+  value_columns = _init_value_columns()
   df_emo = pd.read_csv(input_path)
 
   # 1) add a column called "aggrId" that contains pd.NA in every row
   df_emo["aggrId"] = pd.NA
-  # 2) sum up all float values of the columns contained in value_columns, divide it by len(value_columns) and store it in a new column called "value"
-  df_emo["value"] = df_emo[VALUE_COLUMNS].sum(axis=1) / len(VALUE_COLUMNS)
-  # 3) add a column called "category" that contains "emotional" in every row (no category used for emotional data)
+  # 2) add a column called "category" that contains "emotional" in every row (no category used for emotional data)
   df_emo["category"] = "emotional"
+  # 3) sum up all float values of the columns contained in value_columns, divide it by len(value_columns) and store it in a new column called "value"
+  df_emo["value"] = df_emo[value_columns].sum(axis=1) / len(value_columns)
   # 4) add a column called "word" that contains the word associated with the iso3 code in the dictionary word_by_country
   df_emo["word"] = df_emo["iso3"].map(word_by_country)    # todo: define na_action?
   # 5) rename the column "iso3" to "country"
   df_emo = df_emo.rename(columns={"iso3": "country"})
   # 6) drop all columns named in value_columns
-  df_emo = df_emo.drop(columns=[col for col in VALUE_COLUMNS if col in df_emo.columns])
-  # 7) reorder columns
-  column_order = ["aggrId", "value", "category", "country", "word"]
-  df_emo = df_emo[column_order]
+  df_emo = df_emo.drop(columns=[col for col in value_columns if col in df_emo.columns])
   
-  # 8) Optiona: drop rows without words
+  # 7) Optional: drop rows without words
   df_emo = df_emo[df_emo["word"].notna()]
 
-  df_emo.to_csv(output_path, index=True, index_label="id")
+  # 8) compute id column
+  df_emo["id"] = np.arange(1, len(df_emo) + 1)
+  # 9) reorder columns
+  column_order = ["id", "aggrId", "value", "category", "country", "word"]
+  df_emo = df_emo[column_order]
+
+  df_emo.to_csv(output_path, index=False)
   print("-done-")
 
 if __name__ == "__main__":
-  BASE_PATH = os.path.join("data")#'..', '..', 'data')
-  DATASET_PATH = os.path.join(BASE_PATH, "dummy", "emotion-values-dummy.csv")
-  WORDS_PATH = os.path.join(BASE_PATH, "dummy", "emotion-mvp-words-dummy.csv")
-  OUTPUT_PATH = os.path.join(BASE_PATH, "dummy", "data_types", "emo.csv")
-  perform(DATASET_PATH, WORDS_PATH, OUTPUT_PATH)
+  parser = argparse.ArgumentParser(
+    prog='Emotional Pain Dataset Generation Script',
+    description='Generates the emotional pain dataset from a sentiment analysis dataset.'
+  )
+  parser.add_argument("-bp", "--base-path", type=str, help="common base path shared among input and output file")
+  parser.add_argument("-i", "-in", "--input", type=str, help="path to the input file")
+  parser.add_argument("-o", "-out", "--output", type=str, help="path to the output file")
+  parser.add_argument("-w", "--word-data", type=str, help="path to the word data (input) file")
+
+  args = parser.parse_args()
+  # parse paths
+  if args.base_path:
+    if args.input:
+      input_path = os.path.join(args.base_path, args.input)
+    else:
+      print("Specifying a base_path requires an input argument but none was provided!")
+      exit(1)
+    if args.output:
+      output_path = os.path.join(args.base_path, args.output)
+    else:
+      print("Specifying a base_path requires an output argument but none was provided!")
+      exit(1)
+    if args.word_data:
+      words_path = os.path.join(args.base_path, args.word_data)
+    else:
+      print("Specifying a base_path requires a word-data argument but none was provided!")
+      exit(1)
+  else:
+    input_path = args.input if args.input else DEFAULT_INPUT_PATH
+    output_path = args.output if args.output else DEFAULT_OUTPUT_PATH
+    words_path = args.word_data if args.word_data else DEFAULT_WORDS_PATH
+    
+  perform(input_path, words_path, output_path)
--- scripts/AIO_prod/gen_socioeco_dataset.py
+++ scripts/AIO_prod/gen_socioeco_dataset.py
@@ -1,12 +1,18 @@
+import argparse
 from typing import Dict
 import os
 import numpy as np
 import pandas as pd
 
+# Constants
+DEFAULT_BASE_PATH = os.path.join("data")
+DEFAULT_INPUT_PATH = os.path.join(DEFAULT_BASE_PATH, 'raw', 'socioeco', 'GDP.csv')
+DEFAULT_OUTPUT_PATH = os.path.join(DEFAULT_BASE_PATH, 'dummy', 'data_types', 'socioeco.csv')
+
 def _compute_gdp_pain(df_gdp: pd.DataFrame) -> pd.Series:
   # (logarithmic relative to the max value)
   log_max_gdp = np.log(df_gdp["gdp"].max())
-  return (log_max_gdp - np.log(df_gdp["gdp"].astype(float))) / log_max_gdp # pain = 1 - log(gdp) / log(maxGdp)
+  return 1 - np.log(df_gdp["gdp"].astype(float)) / log_max_gdp # pain = 1 - log(gdp) / log(maxGdp)
 
 def _generate_gdp_dataset(input_path: str) -> pd.DataFrame:
   # 1) read dataset
@@ -52,9 +58,40 @@ def perform(input_paths: Dict[str, str], output_path: str):
   print("-done-")
 
 if __name__ == "__main__":
-  print("TODO: implement args?")
+  """
+  GDP Info:
+  - Dataset Source = https://datacatalog.worldbank.org/search/dataset/0038130/gdp-ranking
+    - Downloadlink = https://datacatalogfiles.worldbank.org/ddh-published/0038130/DR0046440/GDP.csv
+      - values are given in Million $
+  """
+  parser = argparse.ArgumentParser(
+    prog='Socio-economic Pain Dataset Generation Script',
+    description='Generates the socio-economic pain dataset from GDP data.'
+  )
+  parser.add_argument("-bp", "--base-path", type=str, help="common base path shared among input and output file")
+  parser.add_argument("-i", "-in", "--input", type=str, help="path to the input file")
+  parser.add_argument("-o", "-out", "--output", type=str, help="path to the output file")
 
-  BASE_PATH = os.path.join("data")#'..', '..', 'data')
-  DATASET_PATH = os.path.join(BASE_PATH, 'raw', 'socioeco', 'GDP.csv')
-  OUTPUT_PATH = os.path.join(BASE_PATH, 'dummy', 'data_types', 'socioeco.csv')
-  perform({ "GDP": DATASET_PATH }, OUTPUT_PATH)
+  args = parser.parse_args()
+  # parse paths
+  if args.base_path:
+    if args.input:
+      input_path = os.path.join(args.base_path, args.input)
+    else:
+      print("Specifying a base_path requires an input argument but none was provided!")
+      exit(1)
+    if args.output:
+      output_path = os.path.join(args.base_path, args.output)
+    else:
+      print("Specifying a base_path requires an output argument but none was provided!")
+      exit(1)
+    if args.word_data:
+      words_path = os.path.join(args.base_path, args.word_data)
+    else:
+      print("Specifying a base_path requires a word-data argument but none was provided!")
+      exit(1)
+  else:
+    input_path = args.input if args.input else DEFAULT_INPUT_PATH
+    output_path = args.output if args.output else DEFAULT_OUTPUT_PATH
+
+  perform({ "GDP": input_path }, output_path)
